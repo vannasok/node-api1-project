d29332fac4e3da2b13227705dcf2815ac4fd28bb
vanna sok <vannasok@vannas-MacBook-Pro.local>
2019-10-28 13:20:51 -0700

    tested get request

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 // implement your API here
-console.log('object');
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+
+//parse json from body
+server.use(express.json());
+
+//test the localhost port 5000
+server.get('/', (req, res) => {
+   res.send('test test');
+});
+
+// get request
+server.get('/api/users', (req, res) => {
+   db.find()
+      .then(users => {
+         res.status(200).json(users);
+      })
+      .catch(err => {
+         console.log('get request error', err);
+         res.status(500).json({
+            error: 'The users info could not be retrieved.'
+         });
+      });
+});
+
+// port listen
+const port = 5000;
+server.listen(port, () => {
+   console.log('API RUNNING......');
+});
